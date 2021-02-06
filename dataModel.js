3cb1ae9cc626d52500a1a9bab4a9bc5d68ea288c
iamaeytee <itsaeytee@gmail.com>
2021-02-06 11:15:21 +0530

    Assesment completed

--- dataModel.js
+++ dataModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const DataSchema = mongoose.Schema({
-    date: Date,
+    date: Object,
     method: String,
     headers: Object,
     path: String,
--- route.js
+++ route.js
@@ -5,7 +5,19 @@ const app = express();
 
 app.all('/process/*', (req, res) => {
     const start = process.hrtime.bigint();
-    const date = new Date().toISOString();
+    let isoDate = new Date();
+
+    const day = isoDate.getDate();
+    const month = isoDate.getMonth();
+    const year = isoDate.getFullYear();
+
+    const date = {
+        day,
+        month,
+        year
+    }
+    
+
     
     setTimeout( async () => {
         const end = process.hrtime.bigint();
@@ -19,9 +31,75 @@ app.all('/process/*', (req, res) => {
 
         await Data.create({ date, method, headers, path, query, body, duration });
         
-        res.status(200).json({ date, method, headers, path, query, body, duration: `${duration} seconds` });
+        return res.status(200).json({ date, method, headers, path, query, body, duration: `${duration} seconds` });
     }, 15000 + Math.random() * (30 - 5) * 1000);
 });
 
+app.get('/stats', async (req, res) => {
+    try {
+        const { startDate, endDate, startYear, endYear } = req.query;
+
+        const fromDate= parseInt(startDate);
+        const toDate = parseInt(endDate);
+        const fromYear = parseInt(startYear);
+        const toYear = parseInt(endYear);
+
+    if(Object.keys(req.query).length === 0){
+        const data = await Data.aggregate([
+            {
+                $group: {
+                    _id: '$method',
+                    requestCount: {
+                        $sum: 1
+                    },
+                    averageResponseTime: {
+                        $avg: "$duration"
+                    }
+                }
+            }
+        ]);
+
+        return res.send(data);
+    }
+    else if(req.query && startDate && endDate && startYear && endYear){
+        const data = await Data.aggregate([
+            {
+                $match: {
+                        $and: [{
+                          'date.day': {
+                                $gte: fromDate,
+                                $lte: toDate
+                            }
+                        },
+                        {
+                          'date.year': {
+                                $gte: fromYear,
+                                $lte: toYear
+                            }
+                        }]
+                      }
+                },
+            {
+                $group: {
+                    _id: "$method",
+                    count: {
+                        $sum: 1
+                    },
+                    avg: {
+                        $avg: "$duration"
+                    }
+                }
+            }
+        ]);
+        return res.send(data);
+    }
+    else if(!startDate || !endDate || !startYear || !endYear){
+        return res.send(`If you are filtering based on date, please enter all fields (startDate, endDate, startYear, endYear)`)
+    }
+    } catch (error) {
+        console.log(error);
+        return res.send('something went wrong');
+    }
+});
 
 module.exports = app;
